c7c78c09bc6de25f4157f241892474e21bd5c4f7
Yasmin Lima <99838930+whoismin@users.noreply.github.com>
2023-04-02 23:05:54 -0300

    Add files via upload

---
+++ Formulario-03-04/consulta.php
@@ -0,0 +1,83 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+  </head>
+  <body style="background-color: rgb(202, 202, 202);">
+      <div class="container" style="background-color: white; padding: 0;">
+     <nav class="navbar navbar-expand-lg bg-primary"  data-bs-theme="dark">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">SISTEMA WEB</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Cadastrar</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Consultar</a>
+              </li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <div style='margin-left:40px; margin-right:40px;'>
+        <p>
+            
+        <font size ="5">
+        <h3><center> Consultar- Contatos Agendados</center></h1>
+        </font>
+          
+          <div class=" table-2">
+
+                        <div class=" bg-primary ">
+                            <h5>Nome</h5>
+                        </div>
+                        <div class="bg-primary ">
+                            <h5>Telefone</h5>
+                        </div class="bg-primary ">
+                        <div class="bg-primary ">
+
+                            <h5>Origem</h5>
+                        </div>
+                        <div class="bg-primary ">
+
+                            <h5>Contato</h5>
+                        </div>
+                        <div class="bg-primary ">
+
+                            <h5>Observação</h5>
+                        </div>
+                        <div class="bg-primary ">
+
+                            <h5>Ação</h5>
+
+                          
+                </table>   
+            </div>
+        <br>
+        <br>
+        <br>      
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+</body>
+
+</html>
+            
+          
+      </div>
+      
+</html>
